aca198874fd2ee0bfa517aca90d0fcc8b2b6ba13
Sam-Thakas <sw.waithakasamuel@gmail.com>
2026-03-03 12:46:11 +0300

    Fetch and display Pokémon list from API

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,33 @@
-import { Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { ScrollView, Text, View } from "react-native";
 
 export default function Index() {
+  const [pokemons, setPokemons] = useState<{ name: string }[]>([]);
+
+  useEffect(() => {
+    fetchPokemons();
+  }, []);
+
+  async function fetchPokemons() {
+    try {
+      const response = await fetch(
+        "https://pokeapi.co/api/v2/pokemon/?limit=20",
+      );
+      const data = await response.json();
+      setPokemons(data.results);
+      console.log(data.results);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <ScrollView>
+      {pokemons.map((pokemon) => (
+        <View key={pokemon.name}>
+          <Text>{pokemon.name}</Text>
+        </View>
+      ))}
+    </ScrollView>
   );
 }
