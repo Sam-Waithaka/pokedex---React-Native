625c20b7be80aee8b3893e6e07598a89411a3d64
Sam-Thakas <sw.waithakasamuel@gmail.com>
2026-03-03 13:33:56 +0300

    Enhance Pokémon details display by adding images and types

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,23 @@
 import { useEffect, useState } from "react";
-import { ScrollView, Text, View } from "react-native";
+import { ScrollView, Text, View, Image, StyleSheet } from "react-native";
 
 interface Pokemon{
   name: string,
   image: string
+  imageBack: string
+  types: PokemonType[]
+}
+
+interface PokemonType {
+  type: {
+    name: string, 
+    url: string
+  }
 }
 
 export default function Index() {
-  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+  const [pokemons, setPokemons] = useState<Pokemon[]>([])
+  console.log(JSON.stringify(pokemons[0], null, 2))
 
   useEffect(() => {
     fetchPokemons();
@@ -27,7 +37,9 @@ export default function Index() {
 
           return {
             name: pokemon.name,
-            image: details.sprites.front_default
+            image: details.sprites.front_default,
+            imageBack: details.sprites.back_default,
+            types: details.types
           }
         })
       )
@@ -45,7 +57,19 @@ export default function Index() {
       {pokemons.map((pokemon) => (
         <View key={pokemon.name}>
           <Text>{pokemon.name}</Text>
-          <Text>{pokemon.image}</Text>
+          <Text>{pokemon.types[0].type.name}</Text>
+          <View style={{
+            flexDirection: 'row'
+          }}>
+            <Image 
+              source={{uri: pokemon.image}}
+              style={{width: 150, height: 150}}
+            />
+            <Image 
+              source={{uri: pokemon.imageBack}}
+              style={{width: 150, height: 150}}
+            />
+          </View>
         </View>
       ))}
     </ScrollView>
