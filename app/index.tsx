4910bf01241f4380f34f3df496e22e06807c5f25
Sam-Thakas <sw.waithakasamuel@gmail.com>
2026-03-03 14:11:40 +0300

    Add color mapping for Pokémon types and enhance layout in Index component

--- app/index.tsx
+++ app/index.tsx
@@ -15,6 +15,29 @@ interface PokemonType {
   }
 }
 
+const colorsByType = {
+  bug: '#A6B91A',
+  dark: '#705746',
+  dragon: '#6F35FC',
+  electric: '#F7D02C',
+  fairy: '#D685AD',
+  fighting: '#C22E28',
+  fire: '#EE8130',
+  flying: '#A98FF3',
+  ghost: '#735797',
+  grass: '#7AC74C',
+  ground: '#E2BF65',
+  ice: '#96D9D6',
+  normal: '#A8A77A',
+  poison: '#A33EA1',
+  psychic: '#F95587',
+  rock: '#B6A136',
+  steel: '#B7B7CE',
+  water: '#6390F0',
+  stellar: '#40B5A5' // Also represented as a rainbow/multicolor aura
+};
+
+
 export default function Index() {
   const [pokemons, setPokemons] = useState<Pokemon[]>([])
   console.log(JSON.stringify(pokemons[0], null, 2))
@@ -53,9 +76,19 @@ export default function Index() {
   }
 
   return (
-    <ScrollView>
+    <ScrollView
+      contentContainerStyle={{
+        gap: 16,
+        padding: 16
+      }}
+    >
       {pokemons.map((pokemon) => (
-        <View key={pokemon.name}>
+        <View key={pokemon.name} style={{
+          // @ts-ignore
+          backgroundColor: colorsByType[pokemon.types[0].type.name] + 50,
+          padding: 20,
+          borderRadius: 20
+        }}>
           <Text style={styles.name}>{pokemon.name}</Text>
           <Text style={styles.type}>{pokemon.types[0].type.name}</Text>
           <View style={{
@@ -80,10 +113,12 @@ const styles = StyleSheet.create({
   name: {
     fontSize: 28,
     fontWeight: 'bold',
+    textAlign: 'center'
   },
   type: {
     fontSize: 20,
     fontWeight: 'bold',
-    color: 'grey'
+    color: 'grey',
+    textAlign: 'center'
   }
 })
